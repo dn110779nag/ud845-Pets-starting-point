a7db956a7baf576057735ab50f564e35ec9a775d
novax00-samsung <dn110779nag@gmail.com>
2017-08-23 23:07:11 +0300

    Cursor loader

--- app/src/main/java/com/example/android/pets/CatalogActivity.java
+++ app/src/main/java/com/example/android/pets/CatalogActivity.java
@@ -15,26 +15,23 @@
  */
 package com.example.android.pets;
 
-import android.app.LoaderManager;
-import android.content.ContentValues;
-import android.content.CursorLoader;
+
 import android.content.Intent;
-import android.content.Loader;
 import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
-import android.widget.TextView;
 
-import com.example.android.pets.data.PetContract;
+import com.example.android.pets.data.PetContract.PetEntry;
 import com.example.android.pets.data.PetCursorAdapter;
 import com.example.android.pets.data.PetDbHelper;
-import com.example.android.pets.data.PetContract.PetEntry;
 
 import static com.example.android.pets.data.PetContract.CONTENT_URI;
 
@@ -46,11 +43,6 @@ public class CatalogActivity extends AppCompatActivity implements LoaderManager.
     private PetDbHelper petDbHelper;
     private PetCursorAdapter adapter;
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        displayDatabaseInfo();
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -74,7 +66,8 @@ public class CatalogActivity extends AppCompatActivity implements LoaderManager.
         lvItems.setEmptyView(emptyView);
         adapter = new PetCursorAdapter(this, null);
         lvItems.setAdapter(adapter);
-        displayDatabaseInfo();
+        getSupportLoaderManager().initLoader(0, null, this);
+//        displayDatabaseInfo();
     }
 
 
@@ -107,39 +100,15 @@ public class CatalogActivity extends AppCompatActivity implements LoaderManager.
         getContentResolver().insert(
                 CONTENT_URI,
                 petDbHelper.createValues("Toto", "Terrier", PetEntry.GENDER_MALE, 7));
-        displayDatabaseInfo();
     }
 
 
 
-    /**
-     * Temporary helper method to display information in the onscreen TextView about the state of
-     * the pets database.
-     */
-    private void displayDatabaseInfo() {
-        // Create and/or open a database to read from it
-//        SQLiteDatabase db = petDbHelper.getReadableDatabase();
-
-        // Perform this raw SQL query "SELECT * FROM pets"
-        // to get a Cursor that contains all rows from the pets table.
-//        Cursor cursor = db.query(
-//                PetEntry.TABLE_NAME,
-//                null,
-//                null, null, null, null, PetEntry.COLUMN_PET_NAME);
-//        Cursor cursor = getContentResolver().query(PetContract.CONTENT_URI,
-//                null, null, null,
-//                PetEntry.COLUMN_PET_NAME);
-
-
-
-        getLoaderManager().initLoader(0, null, this);
-
-    }
 
 
 
     @Override
-    public Loader onCreateLoader(int id, Bundle args) {
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
 //        Cursor cursor = getContentResolver().query(PetContract.CONTENT_URI,
 //                null, null, null,
 //                PetEntry.COLUMN_PET_NAME);
